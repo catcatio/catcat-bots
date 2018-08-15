1f13b86fbb15d63e6dd5e8969ec798945966ec55
NiponChi <nipon.chi@gmail.com>
2018-08-16 01:27:18 +0700

    Use dialogflow sdk instead of direct call

--- src/api/line/index.js
+++ src/api/line/index.js
@@ -1,34 +1,49 @@
 const line = require('@line/bot-sdk')
 const eventHandlers = require('./eventHandlers')
+const structjson = require('../dialogflow/structjson')
 
 const projectId = 'catcatchatbot' //https://dialogflow.com/docs/agents#settings
 const sessionId = 'quickstart-session-id'
-// const dialogflow = require('dialogflow').v2beta1
-// const sessionClient = new dialogflow.SessionsClient()
-// const sessionPath = sessionClient.sessionPath(projectId, sessionId)
+const dialogflow = require('dialogflow').v2beta1
+const sessionClient = new dialogflow.SessionsClient()
+const sessionPath = sessionClient.sessionPath(projectId, sessionId)
 
 const dummyMiddleware = (req, res, next) => next()
 
 const middleware = (config) => config.isProduction ? line.middleware(config) : dummyMiddleware
 
-const eventHandler = (config) => {
-  const dialogflow = require('../dialogflow')(config)
+const eventHandler = () => {
+  // const dialogflow = require('../dialogflow')(config)
   return async (event) => {
     // console.log(JSON.stringify(event))
     return eventHandlers(event).then(e => {
-      const requestPayload = {
+      const queryParamsPayload = {
         source: 'line',
         data: event
       }
-      return dialogflow
-        .detectIntent(projectId, sessionId, e.text, e.languageCode, requestPayload)
+      const query = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: e.text,
+            languageCode: e.languageCode
+          },
+        },
+        queryParams: {
+          // https://github.com/dialogflow/dialogflow-nodejs-client-v2/issues/9
+          payload: structjson.jsonToStructProto(queryParamsPayload)
+        }
+      }
+      console.log(JSON.stringify(query, null, 2))
+
+      return sessionClient.detectIntent(query)
         .then(response => {
           console.log('Detected intent result')
           console.log(JSON.stringify(response, null, 2))
           console.log('Detected intent')
-          const result = response.queryResult
+          const result = response[0].queryResult
           console.log(`  Query: ${result.queryText}`)
-          console.log(`  Response: ${result.fulfillmentMessages.map(f => f.text.text).join(',  ')}`)
+          console.log(`  Response: ${result.fulfillmentText}`)
           if (result.intent) {
             console.log(`  Intent: ${result.intent.displayName}`)
           } else {
