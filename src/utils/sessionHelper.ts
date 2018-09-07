3cf80f8b78aec0fea160fc720e5916c43784fbdd
NiponChi <nipon.chi@gmail.com>
2018-09-07 08:02:05 +0700

    Use userid as sessionid and let firebase design if it is a new session

--- src/utils/sessionHelper.ts
+++ src/utils/sessionHelper.ts
@@ -1,12 +1,12 @@
 const pad = (n, width, z = '0') => {
-  n = n + '';
-  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  n = n + ''
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n
 }
 
 const makeSessionId = (event) => {
   const now = new Date()
   try {
-    return `${event.source.userId}-${now.getFullYear()}-${pad(now.getMonth(), 2)}-${pad(now.getDate(), 2)}T00:00:00.000Z`
+    return `${event.source.userId}`
   } catch (err) {
     return `${now.getFullYear()}-${pad(now.getMonth(), 2)}-${pad(now.getDate(), 2)}T00:00:00.000Z`
   }
