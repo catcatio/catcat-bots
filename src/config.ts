16be3e10f953685e3c736a62a1f82df745784f73
NiponChi <nipon.chi@gmail.com>
2018-10-25 11:22:40 +0700

    Setup line pay in config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,25 @@ const port = parseInt(process.env.PORT || '') || 3000
 const chatbots = {
   port
 }
+
+const extendLinepay = (config) => {
+  const linePay = require('line-pay')
+  const linepay = new linePay(config.linepay)
+  config.linepay = linepay
+}
+
 const gdh = require(join(process.cwd(), 'gdh.config.json'))
 const bookshelf = require(join(process.cwd(), 'bookshelf.config.json'))
+const unicorn = require(join(process.cwd(), 'unicorn.config.json'))
+
+extendLinepay(gdh)
+extendLinepay(bookshelf)
+extendLinepay(unicorn)
+
 export default {
   isProduction: process.env.NODE_ENV === 'production',
   chatbots,
   gdh,
-  bookshelf
+  bookshelf,
+  unicorn
 }
--- src/gdh/index.ts
+++ src/gdh/index.ts
@@ -2,10 +2,6 @@ import messageHandler from './messageHandler'
 import fulfillmentHandler from './fulfillmentHandler'
 
 export default async (config) => {
-  const linePay = require('line-pay')
-  const linepay = new linePay(config.linepay)
-  config.linepay = linepay
-
   return {
     name: 'gdh',
     messageHandler: messageHandler(config),
